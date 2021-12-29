4b223fb7c9fd3a6c2dca6ddbf1808f4652d50e63
WOOAK <81667989+WOOAK@users.noreply.github.com>
2021-12-29 10:25:51 +0800

    Add files via upload

---
+++ Day 2 - Life in Weeks/main.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_num = int(age)
+year = 90 - age_num
+x = year * 365
+y = year * 52
+z = year * 12
+print(f"You have {x} days, {y} weeks, and {z} months left.")
